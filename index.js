752da5e22a26839e27a8bfa545b2488df3f20011
Yosef <shmuli5770@gmail.com>
2021-04-05 16:26:59 -0400

    commit tree 00387569c34e7006754687824c1e41c068f6e2ce

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function didThyWin(element, index, array) {
+    if (element.result == 'W') {
+        return true;
+    }
+  }
 
+function superbowlWin(array) {
+    let result = array.find(didThyWin)
+    if (result != undefined) {
+        result = result.year
+    }
+    return result
+}
